c144de9be4daf5640d93ddcb13fa633e34ece795
NANDHANA K <n95663@gmail.com>
2026-08-31 14:56:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,25 @@ function typeEffect() {
 }
 
 typeEffect();
+/* ================= SCROLL REVEAL ================= */
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+
+    reveals.forEach((element) => {
+
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
